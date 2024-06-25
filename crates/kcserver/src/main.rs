eb0aa97f53830110466670a15a17c71b232a1429
Jonathan McPherson <jonathan@rstudio.com>
2024-06-25 14:41:25 -0700

    api stubs (WIP)

--- crates/kcserver/src/main.rs
+++ crates/kcserver/src/main.rs
@@ -2,10 +2,12 @@
 //!
 //! Kallichore server
 
-use std::convert::Infallible;
+use std::{borrow::BorrowMut, convert::Infallible};
 use std::net::SocketAddr;
-use hyper::{Body, Request, Response, Server};
-use hyper::service::{make_service_fn, service_fn};
+use hyper::{service::make_service_fn, Body, Request, Response, Server};
+use openapi_client::server::{MakeService, Service};
+
+mod api;
 
 async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(Response::new("Hello, World".into()))
@@ -16,18 +18,15 @@ async fn main() {
     // We'll bind to 127.0.0.1:3000
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    // A `Service` is needed for every connection, so this
-    // creates one from our `hello_world` function.
-    let make_svc = make_service_fn(|_conn| async {
-        // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(hello_world))
-    });
+    use crate::api::KallichoreApi;
+    let api = KallichoreApi;
+    //let api_impl = Service::new(KallichoreApi);
+    let make_service = MakeService::new(api);
 
-    let server = Server::bind(&addr).serve(make_svc);
+    let server = Server::bind(&addr).serve(make_service);
 
     // Run this server for... forever!
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
     }
 }
-
