e097f55bd31481717f739202aef1e1c4971e1373
Jonathan McPherson <jonathan@rstudio.com>
2024-08-30 14:25:28 -0700

    add some new message types

---
+++ crates/kcshared/src/kernel_message.rs
@@ -0,0 +1,46 @@
+//
+// kernel_message.rs
+//
+// Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+//
+//
+
+use serde::{Deserialize, Serialize};
+
+/// A superset of Jupyter kernel statuses
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum KernelStatus {
+    /// The kernel has not yet started
+    Uninitialized,
+    /// The kernel is in the process of starting
+    Starting,
+    /// The kernel is idle
+    Idle,
+    /// The kernel is busy
+    Busy,
+    /// The kernel is offline (it has not responded to a heartbeat message in the expected time)
+    Offline,
+    /// The kernel has exited
+    Exited,
+}
+
+/// An event that indicates that the kernel has exited
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum KernelExited {
+    /// The kernel exited with a specific exit code
+    Code(i32),
+}
+
+/// Messages that are sent from Kallichore to the client about the kernel
+/// itself. For messages bridging the Jupyter protocol, see `JupyterMessage`.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum KernelMessage {
+    /// The kernel's status has changed
+    Status(KernelStatus),
+
+    /// The kernel has exited
+    Exited(KernelExited),
+}
---
+++ crates/kcshared/src/websocket_message.rs
@@ -0,0 +1,30 @@
+//
+// websocket_message.rs
+//
+// Copyright (C) 2024 Posit Software, PBC. All rights reserved.
+//
+//
+
+use serde::{Deserialize, Serialize};
+
+use crate::{jupyter_message::JupyterMessage, kernel_message::KernelMessage};
+
+/// A message sent over a WebSocket connection. This message can be either a
+/// Jupyter message (conforming roughly to the Jupyter kernel protocol) or a
+/// kernel message (sent from the Kallichore control plane concerning the kernel
+/// itself).
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "kind")]
+pub enum WebsocketMessage {
+    /// Kernel messages are messages about the kernel itself that are outside
+    /// the bounds of the Juptyer protocol, such as startup/shutdown messages
+    /// and interstitial status messages.
+    #[serde(rename = "kernel")]
+    Kernel(KernelMessage),
+
+    /// Jupyter messages are messages that conform to the Jupyter protocol. They
+    /// are not parsed by Kallichore, but are passed through to the Jupyter
+    /// client.
+    #[serde(rename = "jupyter")]
+    Jupyter(JupyterMessage),
+}
